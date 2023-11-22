460db5e3dddf61e86151fc14406d43089ebea067
FuzimakiSyunta <k022g0033@g.neec.ac.jp>
2023-11-22 12:07:20 +0900

    途中

--- Particle.VS.hlsl
+++ Particle.VS.hlsl
@@ -9,7 +9,7 @@ StructuredBuffer<TransformationMatrix> gTransformationMatrices : register(t0);
 struct VertexShaderInput
 {
     float32_t4 position : POSITION0;
-   // float32_t2 texcoord : TEXCOORD0;
+   //float32_t2 texcoord : TEXCOORD0;
 };
 VertexShaderOutput main(VertexShaderInput input, uint32_t instanceId : SV_InstanceID)
 {
