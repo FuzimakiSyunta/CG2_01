095c861b14768c7f1108270c34fa6ed21ce98df2
FuzimakiSyunta <115062898+FuzimakiSyunta@users.noreply.github.com>
2024-01-09 18:28:33 +0900

    おわり

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -2,20 +2,21 @@
 
 struct Material {
 	float32_t4 color;
-};
-
-struct PixelShaderOutput {
-	float32_t4 color : SV_TARGET0;
+	int32_t enableLighting;
 };
 
 ConstantBuffer<Material> gMaterial : register(b0);
-Texture2D<float32_t4> gTexture : register(t0);
+struct PixcelShaderOutput {
+	float32_t4 color : SV_TARGET0;
+};
+
+Texture2D<float32_t4>gTexture : register(t0);
 SamplerState gSampler : register(s0);
 
-PixelShaderOutput main(VertexShaderOutput input) {
-	PixelShaderOutput output;
+
+PixcelShaderOutput main(VertexShaderOutput input) {
 	float32_t4 textureColor = gTexture.Sample(gSampler, input.texcoord);
+	PixcelShaderOutput output;
 	output.color = gMaterial.color * textureColor;
 	return output;
 }
-
--- Object3d.VS.hlsl
+++ Object3d.VS.hlsl
@@ -2,20 +2,26 @@
 
 struct TransformationMatrix {
 	float32_t4x4 WVP;
+	float32_t4x4 World;
 };
-StructuredBuffer<TransformationMatrix> gTransformationMatrices : register(t0);
+
+struct DirectionalLight {
+	float32_t4 color;//< ライトの色
+	float32_t3 direction;//!< ライトの向き
+	float intensity;//< 輝度
+};
+
+ConstantBuffer<TransformationMatrix> gTransformationMatrix : register(b0);
 
 struct VertexShaderInput {
 	float32_t4 position : POSITION0;
 	float32_t2 texcoord : TEXCOORD0;
+	float32_t3 normal : NORMAL0;
 };
+
 VertexShaderOutput main(VertexShaderInput input) {
-
 	VertexShaderOutput output;
-	output.position = Mul(input.position, gTransformationMatrices[instanceId].WVP);
+	output.position = mul(input.position, gTransformationMatrix.WVP);
 	output.texcoord = input.texcoord;
-	output.normal = normalize(mul(input.normal, (float32_t3x3)gTransformationMatrices[instanceId].World));
-
 	return output;
 }
-
